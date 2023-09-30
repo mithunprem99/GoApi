e3f9028aa1c5aa528a4dbcdef528e0ed79ee6e1a
Mithun Prem <mithun@hamon.in>
2023-09-30 18:31:00 +0530

    Api for GET POST

--- main.go
+++ main.go
@@ -22,18 +22,20 @@ var books = []book{
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
-func createBook(c *gin.Context){
+func createBook(c *gin.Context) {
 	var newBook book
 
-	if err := c.BindJSON(&newBook); err !=nil{ 
-		return 
+	if err := c.BindJSON(&newBook); err != nil {
+		return
 	}
-	books = append(books,newBook)
-	c.IndentedJSON(http.StatusCreated,newBook)
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+
 	router.Run(":8080")
 }
